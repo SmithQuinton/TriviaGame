1c59a2f2552b7704b422b50029a22609dd148532
Quinton Smith <smith.quinton42@gmail.com>
2019-07-10 13:02:58 -0400

    changed the frot page, can't get start button to go away

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -35,3 +35,39 @@
 //             image: "<img src='assets/images/knicks.jpeg' class='image'",
 //         }
 //     ];
+$("#start").on("click", function(){
+    for (var i = 0; i < questions.length; i++){
+        $("#subwrapper").append("<h2>" + questions[i].question + "</h2>");
+        for (var j = 0; j < questions[i].choices.length; j++){
+            $("#subwrapper").append("<input type='radio' name='question-'" + i + "' value='" + questions[i].choices[j] +  "'>'" + questions[i].choices[j])
+        }
+    };
+})
+
+var questions = [
+            {
+                question: "What NBA franchise has the most championships?",
+                choices: ["Los Angeles Lakers", "Golden State Warriors", "Boston Celtics", "Chicago Bulls"],
+                answer: "Boston Celtics",
+            },
+            {
+                question: "Which NBA player has the most points scored in a career?",
+                choices: ["Kareem Abdul-Jabbar", "Karl Malone", "Kobe Bryant", "LeBron James"],
+                answer: "Kareem Abdul-Jabbar",
+            },
+            {
+                question: "What player appeared in the most career NBA games?",
+                choices: ["Dirk Nowitzki", "John Stockton", "Robert Parish", "Vince Carter"],
+                answer: "Robert Parish",
+            },
+            {
+                question: "What NBA player has made the most 3-pointers?",
+                choices: ["Ray Allen", "Stephen Curry", "Kyle Korver", "Reggie Miller"],
+                answer: "Ray Allen",
+            },
+            {
+                question: "What team is the laughing stock of the NBA",
+                choices: ["New York Knicks", "New York Knicks", "New York Knicks", "New York Knicks"],
+                ansswer: "New York Knicks",
+            }
+        ];
