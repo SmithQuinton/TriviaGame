bad9a5cd0e88215ed702c311aa1a08db4ec0ab9a
Quinton Smith <smith.quinton42@gmail.com>
2019-07-09 22:53:22 -0400

    started from scratch, still started wrong, at least all the questions are written out and buttons work

---
+++ assets/javascript/app.js
@@ -0,0 +1,37 @@
+// $(document).ready(function() {
+//     var questionCounter = 0;
+//     var time = 15;
+//     var correctGuess = 0;
+//     var wrongGuess = 0;
+//     var questions = [
+//         {
+//             question: "What NBA franchise has the most championships?",
+//             choices: ["Los Angeles Lakers", "Golden State Warriors", "Boston Celtics", "Chicago Bulls"],
+//             answer: "Boston Celtics",
+//             image: "<img src='assets/images/boston-celtics-championships.jpg' class='image'>"
+//         },
+//         {
+//             question: "Which NBA player has the most points scored in a career?",
+//             choices: ["Kareem Abdul-Jabbar", "Karl Malone", "Kobe Bryant", "LeBron James"],
+//             answer: "Kareem Abdul-Jabbar",
+//             image: "<img src='assets/images/Kareem.jpg' class='image'>",
+//         },
+//         {
+//             question: "What player appeared in the most career NBA games?",
+//             choices: ["Dirk Nowitzki", "John Stockton", "Robert Parish", "Vince Carter"],
+//             answer: "Robert Parish",
+//             image: "<img src='assets/images/Robert-Parish.jpg' class='image'",
+//         },
+//         {
+//             question: "What NBA player has made the most 3-pointers?",
+//             choices: ["Ray Allen", "Stephen Curry", "Kyle Korver", "Reggie Miller"],
+//             answer: "Ray Allen",
+//             image: "<img src='assets/images/Ray-Allen.jpg' class='image'",
+//         },
+//         {
+//             question: "What team is the laughing stock of the NBA",
+//             choices: ["New York Knicks", "New York Knicks", "New York Knicks", "New York Knicks"],
+//             ansswer: "New York Knicks",
+//             image: "<img src='assets/images/knicks.jpeg' class='image'",
+//         }
+//     ];
