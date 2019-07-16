7ce2a893b19fdb2670008485e9037f66522c9c59
Quinton Smith <smith.quinton42@gmail.com>
2019-07-16 01:11:17 -0400

    added timer that counts down

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -35,39 +35,57 @@
 //             image: "<img src='assets/images/knicks.jpeg' class='image'",
 //         }
 //     ];
-$("#start").on("click", function(){
-    for (var i = 0; i < questions.length; i++){
-        $("#subwrapper").append("<h2>" + questions[i].question + "</h2>");
-        for (var j = 0; j < questions[i].choices.length; j++){
-            $("#subwrapper").append("<input type='radio' name='question-'" + i + "' value='" + questions[i].choices[j] +  "'>'" + questions[i].choices[j])
-        }
-    };
+$("#start").on("click", function () {
+    game.start();
 })
 
-var questions = [
-            {
-                question: "What NBA franchise has the most championships?",
-                choices: ["Los Angeles Lakers", "Golden State Warriors", "Boston Celtics", "Chicago Bulls"],
-                answer: "Boston Celtics",
-            },
-            {
-                question: "Which NBA player has the most points scored in a career?",
-                choices: ["Kareem Abdul-Jabbar", "Karl Malone", "Kobe Bryant", "LeBron James"],
-                answer: "Kareem Abdul-Jabbar",
-            },
-            {
-                question: "What player appeared in the most career NBA games?",
-                choices: ["Dirk Nowitzki", "John Stockton", "Robert Parish", "Vince Carter"],
-                answer: "Robert Parish",
-            },
-            {
-                question: "What NBA player has made the most 3-pointers?",
-                choices: ["Ray Allen", "Stephen Curry", "Kyle Korver", "Reggie Miller"],
-                answer: "Ray Allen",
-            },
-            {
-                question: "What team is the laughing stock of the NBA",
-                choices: ["New York Knicks", "New York Knicks", "New York Knicks", "New York Knicks"],
-                ansswer: "New York Knicks",
+var questions = [{
+        question: "What NBA franchise has the most championships?",
+        choices: ["Los Angeles Lakers", "Golden State Warriors", "Boston Celtics", "Chicago Bulls"],
+        answer: "Boston Celtics",
+    },
+    {
+        question: "Which NBA player has the most points scored in a career?",
+        choices: ["Kareem Abdul-Jabbar", "Karl Malone", "Kobe Bryant", "LeBron James"],
+        answer: "Kareem Abdul-Jabbar",
+    },
+    {
+        question: "What player appeared in the most career NBA games?",
+        choices: ["Dirk Nowitzki", "John Stockton", "Robert Parish", "Vince Carter"],
+        answer: "Robert Parish",
+    },
+    {
+        question: "What NBA player has made the most 3-pointers?",
+        choices: ["Ray Allen", "Stephen Curry", "Kyle Korver", "Reggie Miller"],
+        answer: "Ray Allen",
+    },
+    {
+        question: "What team is the laughing stock of the NBA",
+        choices: ["New York Knicks", "New York Knicks", "New York Knicks", "New York Knicks"],
+        ansswer: "New York Knicks",
+    }
+];
+var game = {
+    correct: 0,
+    incorrect: 0,
+    counter: 120,
+    countdown: function () {
+        game.counter--;
+        $("#counter").html(game.counter);
+        if (game.counter === 0) {
+            console.log("time is up");
+            game.done();
+        }
+    },
+    start: function () {
+        timer = setInterval(game.countdown,1000); 
+        $("#subwrapper").prepend("<h2>Time Remaining: <span id='counter'>120</span> Seconds </h2>");
+        $("#start").remove();
+        for (var i = 0; i < questions.length; i++) {
+            $("#subwrapper").append("<h2>" + questions[i].question + "</h2>");
+            for (var j = 0; j < questions[i].choices.length; j++) {
+                $("#subwrapper").append("<input type='radio' name='question-'" + i + "' value='" + questions[i].choices[j] + "'>'" + questions[i].choices[j])
             }
-        ];
+        };
+    }
+}
