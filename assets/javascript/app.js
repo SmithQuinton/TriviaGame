acc4dea6f1d4b536966e541af1dce03949d95c6a
Quinton Smith <smith.quinton42@gmail.com>
2019-08-01 19:51:12 -0400

    still having result error

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,91 +1,110 @@
-// $(document).ready(function() {
-//     var questionCounter = 0;
-//     var time = 15;
-//     var correctGuess = 0;
-//     var wrongGuess = 0;
-//     var questions = [
-//         {
-//             question: "What NBA franchise has the most championships?",
-//             choices: ["Los Angeles Lakers", "Golden State Warriors", "Boston Celtics", "Chicago Bulls"],
-//             answer: "Boston Celtics",
-//             image: "<img src='assets/images/boston-celtics-championships.jpg' class='image'>"
-//         },
-//         {
-//             question: "Which NBA player has the most points scored in a career?",
-//             choices: ["Kareem Abdul-Jabbar", "Karl Malone", "Kobe Bryant", "LeBron James"],
-//             answer: "Kareem Abdul-Jabbar",
-//             image: "<img src='assets/images/Kareem.jpg' class='image'>",
-//         },
-//         {
-//             question: "What player appeared in the most career NBA games?",
-//             choices: ["Dirk Nowitzki", "John Stockton", "Robert Parish", "Vince Carter"],
-//             answer: "Robert Parish",
-//             image: "<img src='assets/images/Robert-Parish.jpg' class='image'",
-//         },
-//         {
-//             question: "What NBA player has made the most 3-pointers?",
-//             choices: ["Ray Allen", "Stephen Curry", "Kyle Korver", "Reggie Miller"],
-//             answer: "Ray Allen",
-//             image: "<img src='assets/images/Ray-Allen.jpg' class='image'",
-//         },
-//         {
-//             question: "What team is the laughing stock of the NBA",
-//             choices: ["New York Knicks", "New York Knicks", "New York Knicks", "New York Knicks"],
-//             ansswer: "New York Knicks",
-//             image: "<img src='assets/images/knicks.jpeg' class='image'",
-//         }
-//     ];
 $("#start").on("click", function () {
-    game.start();
+    game.start();  
 })
 
 var questions = [{
         question: "What NBA franchise has the most championships?",
-        choices: ["Los Angeles Lakers", "Golden State Warriors", "Boston Celtics", "Chicago Bulls"],
-        answer: "Boston Celtics",
+        choices: [" Los Angeles Lakers", " Golden State Warriors", " Boston Celtics", " Chicago Bulls"],
+        answer: " Boston Celtics",
     },
     {
         question: "Which NBA player has the most points scored in a career?",
-        choices: ["Kareem Abdul-Jabbar", "Karl Malone", "Kobe Bryant", "LeBron James"],
-        answer: "Kareem Abdul-Jabbar",
+        choices: [" Kareem Abdul-Jabbar", " Karl Malone", " Kobe Bryant", " LeBron James"],
+        answer: " Kareem Abdul-Jabbar",
     },
     {
         question: "What player appeared in the most career NBA games?",
-        choices: ["Dirk Nowitzki", "John Stockton", "Robert Parish", "Vince Carter"],
-        answer: "Robert Parish",
+        choices: [" Dirk Nowitzki", " John Stockton", " Robert Parish", " Vince Carter"],
+        answer: " Robert Parish",
     },
     {
         question: "What NBA player has made the most 3-pointers?",
-        choices: ["Ray Allen", "Stephen Curry", "Kyle Korver", "Reggie Miller"],
-        answer: "Ray Allen",
+        choices: [" Ray Allen", " Stephen Curry", " Kyle Korver", " Reggie Miller"],
+        answer: " Ray Allen",
     },
     {
         question: "What team is the laughing stock of the NBA",
-        choices: ["New York Knicks", "New York Knicks", "New York Knicks", "New York Knicks"],
-        ansswer: "New York Knicks",
+        choices: [" New York Knicks", " New York Knicks", " New York Knicks", " New York Knicks"],
+        ansswer: " New York Knicks",
     }
 ];
 var game = {
     correct: 0,
     incorrect: 0,
-    counter: 120,
+    counter: 10,
     countdown: function () {
         game.counter--;
         $("#counter").html(game.counter);
         if (game.counter === 0) {
             console.log("time is up");
-            game.done();
+            // alert("Time is up");
+            clearInterval(this.countdown);
+            this.done();
         }
     },
     start: function () {
         timer = setInterval(game.countdown,1000); 
-        $("#subwrapper").prepend("<h2>Time Remaining: <span id='counter'>120</span> Seconds </h2>");
+        $("#subwrapper").prepend("<h2>Time Remaining: <span id='counter'>10</span> Seconds </h2>");
         $("#start").remove();
         for (var i = 0; i < questions.length; i++) {
             $("#subwrapper").append("<h2>" + questions[i].question + "</h2>");
             for (var j = 0; j < questions[i].choices.length; j++) {
-                $("#subwrapper").append("<input type='radio' name='question-'" + i + "' value='" + questions[i].choices[j] + "'>'" + questions[i].choices[j])
+                $("#subwrapper").append("<input type='radio' name='question-" + i + "' value='" + questions[i].choices[j] + "'>'" + questions[i].choices[j])
             }
         };
+        $("#subwrapper").append("<br><button id='end'>DONE</button> ")
+    },
+    done: function(){
+        $.each($("input[name = 'question-0]':checked"),function(){
+            if($(this).val() == questions[0].answer){
+                 game.correct++;
+            }
+            else {
+                game.incorrect++; 
+            }
+        });
+        $.each($("input[name = 'question-1]':checked"),function(){
+            if($(this).val() == questions[1].answer){
+                 game.correct++;
+            }
+            else {
+                game.incorrect++; 
+            }
+        });
+        $.each($("input[name = 'question-2]':checked"),function(){
+            if($(this).val() == questions[2].answer){
+                 game.correct++;
+            }
+            else {
+                game.incorrect++; 
+            }
+        });
+        $.each($("input[name = 'question-3]':checked"),function(){
+            if($(this).val() == questions[3].answer){
+                 game.correct++;
+            }
+            else {
+                game.incorrect++; 
+            }
+        });
+        $.each($("input[name = 'question-4]':checked"),function(){
+            if($(this).val() == questions[4].answer){
+                 game.correct++;
+            }
+            else {
+                game.incorrect++; 
+            }
+        });
+
+        this.result();
+    },
+
+    result: function(){
+        clearInterval(timer);
+        $("#subwrapper h2").remove();
+        $("#subwrapper").html("<h2>All Done</h2>");
+        $("#subwrapper").append("<h3>Correct Answers: " + this.correct + "</h3>");
+        $("#subwrapper").append("<h3>Incorrect Answers: " + this.incorrect + "</h3>");
+        $("#subwrapper").append("<h3>Unanswered: " + (questions.length-(this.incorrect+this.correct))  + "</h3>");
     }
 }
